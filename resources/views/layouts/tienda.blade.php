836209137bf869b26e6956dd9f4495bf8c9e5b0d
memosk8 <memolopez9363@gmail.com>
2022-01-07 22:40:45 -0600

    updated navbar and products table

--- resources/views/layouts/tienda.blade.php
+++ resources/views/layouts/tienda.blade.php
@@ -1,271 +1,60 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
     <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <meta name="author" content="Guillermo López">
 
-    <title>Dashboard Template for Bootstrap</title>
+    <title>Fitness Salud </title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/dashboard/">
-
-    <!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
-    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
     <link href="dashboard.css" rel="stylesheet">
 </head>
 
-<body>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Fitness Salud</a>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="#">Sign out</a>
-            </li>
-        </ul>
+<body class="bg-black bg-opacity-50">
+    <nav class="nav navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 m-1 rounded-pill">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0 m-2 text-md-center text-uppercase" href="#">Fitness Salud</a>
+        <div class="nav-item">
+            <a class="nav-link" href="{{ url('/tienda/ventas') }}">
+                <span data-feather="file"></span>
+                Ventas
+            </a>
+        </div>
+
+        <div>
+            <a class="nav-link" href="{{ url('/tienda/productos') }}">
+                <span data-feather="shopping-cart"></span>
+                Productos
+            </a>
+        </div>
+        <div>
+            <a class="nav-link" href="{{ url('/tienda/clientes') }}">
+                <span data-feather="users"></span>
+                Clientes
+            </a>
+        </div>
+        <div>
+
+        </div>
+
+        <input class="form-control form-control-dark w-25 text-center nav-item" type="text" placeholder="Buscar . . ."
+            aria-label="Search">
+
+        <a class="nav-link" href="#">Salir</a>
+
+
     </nav>
 
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                <div class="sidebar-sticky">
-                    <ul class="nav flex-column">
+    <main class="container-fluid">
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/tienda/ventas'); }}">
-                                <span data-feather="file"></span>
-                                Ventas
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/tienda/productos'); }}">
-                                <span data-feather="shopping-cart"></span>
-                                Productos
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="users"></span>
-                                Clientes
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="bar-chart-2"></span>
-                                Reportes
-                            </a>
-                        </li>
+        @yield('main-content')
 
-                    </ul>
-
-                    {{-- <h6
-                        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                        <span>Saved reports</span>
-                        <a class="d-flex align-items-center text-muted" href="#">
-                            <span data-feather="plus-circle"></span>
-                        </a>
-                    </h6> --}}
-
-                    {{-- <ul class="nav flex-column mb-2">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="file-text"></span>
-                                Current month
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="file-text"></span>
-                                Last quarter
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="file-text"></span>
-                                Social engagement
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="file-text"></span>
-                                Year-end sale
-                            </a>
-                        </li>
-                    </ul> --}}
-                </div>
-            </nav>
-
-            {{-- <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-                <div
-                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Ventas</h1>
-                    <div class="btn-toolbar mb-2 mb-md-0">
-                        <div class="btn-group mr-2">
-                            <button class="btn btn-sm btn-outline-secondary">Share</button>
-                            <button class="btn btn-sm btn-outline-secondary">Export</button>
-                        </div>
-                        <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                            <span data-feather="calendar"></span>
-                            This week
-                        </button>
-                    </div>
-                </div> --}}
-
-                {{-- <canvas class="my-4" id="myChart" width="900" height="380"></canvas> --}}
-
-                {{-- <h2>Section title</h2> --}}
-                @yield('main-content')
-                {{-- <div class="table-responsive">
-                    <table class="table table-striped table-sm">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Fecha</th>
-                                <th>Cliente</th>
-                                <th>Vendedor</th>
-                                <th>Total</th>
-                                <th>Productos</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1,001</td>
-                                <td>Lorem</td>
-                                <td>ipsum</td>
-                                <td>dolor</td>
-                                <td>sit</td>
-                                <td>sit</td>
-                            </tr>
-                            <tr>
-                                <td>1,002</td>
-                                <td>amet</td>
-                                <td>consectetur</td>
-                                <td>adipiscing</td>
-                                <td>elit</td>
-                            </tr>
-                            <tr>
-                                <td>1,003</td>
-                                <td>Integer</td>
-                                <td>nec</td>
-                                <td>odio</td>
-                                <td>Praesent</td>
-                            </tr>
-                            <tr>
-                                <td>1,003</td>
-                                <td>libero</td>
-                                <td>Sed</td>
-                                <td>cursus</td>
-                                <td>ante</td>
-                            </tr>
-                            <tr>
-                                <td>1,004</td>
-                                <td>dapibus</td>
-                                <td>diam</td>
-                                <td>Sed</td>
-                                <td>nisi</td>
-                            </tr>
-                            <tr>
-                                <td>1,005</td>
-                                <td>Nulla</td>
-                                <td>quis</td>
-                                <td>sem</td>
-                                <td>at</td>
-                            </tr>
-                            <tr>
-                                <td>1,006</td>
-                                <td>nibh</td>
-                                <td>elementum</td>
-                                <td>imperdiet</td>
-                                <td>Duis</td>
-                            </tr>
-                            <tr>
-                                <td>1,007</td>
-                                <td>sagittis</td>
-                                <td>ipsum</td>
-                                <td>Praesent</td>
-                                <td>mauris</td>
-                            </tr>
-                            <tr>
-                                <td>1,008</td>
-                                <td>Fusce</td>
-                                <td>nec</td>
-                                <td>tellus</td>
-                                <td>sed</td>
-                            </tr>
-                            <tr>
-                                <td>1,009</td>
-                                <td>augue</td>
-                                <td>semper</td>
-                                <td>porta</td>
-                                <td>Mauris</td>
-                            </tr>
-                            <tr>
-                                <td>1,010</td>
-                                <td>massa</td>
-                                <td>Vestibulum</td>
-                                <td>lacinia</td>
-                                <td>arcu</td>
-                            </tr>
-                            <tr>
-                                <td>1,011</td>
-                                <td>eget</td>
-                                <td>nulla</td>
-                                <td>Class</td>
-                                <td>aptent</td>
-                            </tr>
-                            <tr>
-                                <td>1,012</td>
-                                <td>taciti</td>
-                                <td>sociosqu</td>
-                                <td>ad</td>
-                                <td>litora</td>
-                            </tr>
-                            <tr>
-                                <td>1,013</td>
-                                <td>torquent</td>
-                                <td>per</td>
-                                <td>conubia</td>
-                                <td>nostra</td>
-                            </tr>
-                            <tr>
-                                <td>1,014</td>
-                                <td>per</td>
-                                <td>inceptos</td>
-                                <td>himenaeos</td>
-                                <td>Curabitur</td>
-                            </tr>
-                            <tr>
-                                <td>1,015</td>
-                                <td>sodales</td>
-                                <td>ligula</td>
-                                <td>in</td>
-                                <td>libero</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div> --}}
-            </main>
-        </div>
-    </div>
+    </main>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -284,8 +73,7 @@
     <script>
         feather.replace()
     </script>
-{{-- 
-    <!-- Graphs -->
+    {{-- <!-- Graphs -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
     <script>
         var ctx = document.getElementById("myChart");
@@ -315,8 +103,7 @@
                 }
             }
         });
-    </script>
-     --}}
+    </script> --}}
 </body>
 
 </html>
--- resources/views/tienda/productos/index.blade.php
+++ resources/views/tienda/productos/index.blade.php
@@ -2,14 +2,13 @@
 
 @section('main-content')
     <div class="row">
-        <div class="col-md-12">
-            <h1 class="p-3 text-center">Productos en existencia</h1>
+        <div class="col-md-12 p-3">
+            <h1 class=" text-center bg-light p-2 mx-md-5">Productos en existencia</h1>
         </div>
         <div class="col-md-12">
             <div class="card" style="width: 100%">
-                <div class="card-body">
-
-                    @foreach ($productos as $producto)
+                <div class="card-body ">
+                    {{-- @foreach ($productos as $producto)
 
                         <h5 class="card-title pt-4">Precio: {{ $producto->precio }}</h5>
 
@@ -24,7 +23,33 @@
                         </p>
 
 
-                    @endforeach
+                    @endforeach --}}
+
+                    <table class="table table-bordered table-sm table-striped text-center">
+
+                        <thead>
+                            <tr class="bg-info ">
+                                <th scope="col"># ID</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Descripción</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Costo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($productos as $producto)
+                                <tr>
+                                    <th scope="row">{{ $producto->id }}</th>
+                                    <td>{{ $producto->nombre }}</td>
+                                    <td>{{ $producto->desc }}</td>
+                                    <td>{{ $producto->precio }}</td>
+                                    <td>{{ $producto->costo }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+
 
                 </div>
             </div>
